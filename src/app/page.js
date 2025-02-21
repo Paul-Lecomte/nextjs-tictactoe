7909bc6de0975872ddcf11e1ec8817db67f50400
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-21 10:28:32 +0100

    added the first bit of logic to be able to press and to have each player

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,52 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-      <div>
-        <h1>
-          Tic Tac Toe
-        </h1>
-        <div className="game">
-          <div className="game-menu">
-            <p>Turn</p>
-          </div>
-          <div className="game_board">
-            {[...Array(9)].map((v, idx) => {
-              return (
-                  <div
+    const [xTurn, setXTurn] = useState(true);
+    const [boardData, setBoardData] = useState({
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+    });
 
-                      key={idx}
-                      className="square">
-                    X
-                  </div>
-              );
-            })}
-          </div>
+    const updateBoardData = (idx) => {
+        if (!boardData[idx]) {
+            //will check whether specify idx is empty or not
+            let value = xTurn === true ? "X" : "O";
+            setBoardData({ ...boardData, [idx]: value });
+            setXTurn(!xTurn);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Tic Tac Toe</h1>
+            <div className="game">
+                <div className="game__menu">
+                    <p>{xTurn === true ? "X Turn" : "O Turn"}</p>
+                </div>
+                <div className="game__board">
+                    {[...Array(9)].map((v, idx) => {
+                        return (
+                            <div
+
+                                key={idx}
+                                className="square"
+                                onClick={() => {
+                                    updateBoardData(idx);
+                                }}>
+                                {boardData[idx]}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
